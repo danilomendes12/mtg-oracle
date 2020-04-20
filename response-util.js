601fb98dac0bd351248654d9550cdf8686bdfa13
Danilo Mendes <danilo.mendes@bitcapital.com.br>
2020-04-20 16:31:36 -0300

    created embed answer for card name search

--- response-util.js
+++ response-util.js
@@ -1,8 +1,24 @@
+const Discord = require('discord.js');
 
 const formatCardOutputFromResult = (result) => {
-    const { name, mana_cost, oracle_text, reserved, type_line } = result;
+    let { name, mana_cost, oracle_text, reserved, type_line, scryfall_uri, rarity } = result;
+    const imageUrl = result.image_uris.normal;
     
-    return `Name: ${name} \nMana cost: ${mana_cost} \nDescription: ${oracle_text} \nReserved: ${reserved} \nType: ${type_line}`;
+    if(!mana_cost) mana_cost = '0';
+
+    return new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(name)
+            .setURL(scryfall_uri)
+            .setDescription(oracle_text)
+            .addFields(
+                { name: 'Mana Cost', value: mana_cost },
+                { name: 'Type', value: type_line },
+                { name: 'Rarity', value: rarity },
+                { name: 'Reserved', value: reserved }
+            )
+            .setImage(imageUrl)
+            .setTimestamp()
 }
 
 const notFoundMessage = () => {
